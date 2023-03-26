edf5d1ae51298f8408cc217dd5cd7728a25e9643
Vijay Dhyani <vijay97dhyani@gmail.com>
2023-03-26 10:50:19 +0000

    pwa code added

--- next-frontend/next.config.js
+++ next-frontend/next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// module.exports = { reactStrictMode: true };
+// const withPWA = require("next-pwa");
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     disable: process.env.NODE_ENV === "development",
+//     skipWaiting: true,
+//   },
+// });
+
+// github 
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    // disable: process.env.NODE_ENV === "development",
+    skipWaiting: true,
+  },
+})
